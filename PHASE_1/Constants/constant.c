b6320e52bce09b25c34e482c9b0c3bf4b0774df8
MANIKANDAN VV <manikandanvv277@gmail.com>
2026-09-18 10:17:20 +0530

    Update constant.c

--- PHASE_1/Constants/constant.c
+++ PHASE_1/Constants/constant.c
@@ -1,1 +1,14 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  const  int days_in_week = 7;
+  const float pi = 3.14159f;
+
+  printf("NUMBER OF DAYS IN WEEK : %d\n",days_in_week);
+  printf("THE VALUE OF PI IS : %.4xf",pi);
+  return 0;
+
+}
