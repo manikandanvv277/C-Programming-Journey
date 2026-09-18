fbb5c68092188694051576e205341761264550bc
MANIKANDAN VV <manikandanvv277@gmail.com>
2026-09-18 10:32:54 +0530

    Update sample.c
    Updated the program to calculate the area of a circle based on user input for the radius.

--- PHASE_1/Constants/sample.c
+++ PHASE_1/Constants/sample.c
@@ -1,1 +1,30 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  /*const  int days_in_week = 7;
+  const float pi = 3.14159f;
+   pi = 3.45;
+
+  printf("NUMBER OF DAYS IN WEEK : %d\n",days_in_week);
+  printf("THE VALUE OF PI IS : %.4xf",pi);
+  return 0;
+   */
+
+   const float PI = 3.14159f;
+   float radius ;
+   float radius_1;
+
+
+
+   printf("Enter the value of radius ");
+   scanf("%f",&radius);
+
+   radius_1 = PI * radius * radius ;
+
+   printf("The  Radius of circle is : %f",radius_1);
+   return 0;
+
+}
