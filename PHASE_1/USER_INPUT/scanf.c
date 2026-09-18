19508c0f2c482952a7b88c108ebc4bda6f895099
MANIKANDAN VV <manikandanvv277@gmail.com>
2026-09-18 09:22:15 +0530

    Update scanf.c

--- PHASE_1/USER_INPUT/scanf.c
+++ PHASE_1/USER_INPUT/scanf.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+    int age_1;
+    int age_2;
+    printf("Enter your age : ");
+    scanf("%d", &age_1);
+    printf("age for person one  : %d\n ",age_1);
+
+
+
+    printf("Enter your age ");
+    scanf("%d",&age_2);
+
+    printf("age for person 2 : %d ",age_2);
+    return 0;
+}
