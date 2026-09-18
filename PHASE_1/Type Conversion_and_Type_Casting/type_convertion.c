b3b2846e7a67c4c681ac0127e613e88b9f313302
MANIKANDAN VV <manikandanvv277@gmail.com>
2026-09-18 10:40:19 +0530

    Update type_convertion.c
    Added type conversion from int to float in C.

--- PHASE_1/Type Conversion_and_Type_Casting/type_convertion.c
+++ PHASE_1/Type Conversion_and_Type_Casting/type_convertion.c
@@ -1,1 +1,15 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  int num = 10;
+  float result ;
+  result = num ;
+  printf("The value of number is %d \n ",num);
+  printf("The result is : %.2f ", result);
+  return 0;
+
+
+}
